cb411032fec5f27c8de0e9e7c87abf7c2073e656
tgwow <thiago.2809@outlook.com.br>
2020-01-30 23:06:48 -0300

    add control of lineWidth and brushStyle

--- 08 - Fun with HTML5 Canvas/script.js
+++ 08 - Fun with HTML5 Canvas/script.js
@@ -1,14 +1,18 @@
 const canvas = document.querySelector('#draw')
+const input = document.querySelector('input')
+const selector = document.querySelector('select')
+
+console.log(selector.value)
 
 const ctx = canvas.getContext('2d')
 
 canvas.width = window.innerWidth
-canvas.height = window.innerHeight
+canvas.height = window.innerHeight - 15
 ctx.strokeStyle = '#BADA55'
 ctx.lineJoin = 'round'
 ctx.lineCap = 'round'
-ctx.lineWidth = 15
-ctx.globalCompositeOperation = 'source-out'
+ctx.lineWidth = 0
+//ctx.globalCompositeOperation = 'source-out'
 
 let isDrawing = false;
 let lastX = 0
@@ -32,17 +36,44 @@ function draw(e){
 
     if( hue >= 360) hue = 0
 
-    if ((ctx.lineWidth >= 80) || (ctx.lineWidth <= 1)) {
+    if ((ctx.lineWidth >= 40) || (ctx.lineWidth <= 1)) {
         direction = !direction
     }
 
     if (direction) {
-        ctx.lineWidth++
+       // ctx.lineWidth++
     } else {
-        ctx.lineWidth--
+        //ctx.lineWidth--
     }
 }
 
+function handleChangeInput(e) {
+    ctx.lineWidth = this.value
+}
+
+function handleChangeSelect() {
+    ctx.globalCompositeOperation = ''+this.value+''
+    console.log(ctx.globalCompositeOperation)
+}
+
+function getGlobalCompositeOperation() {
+    var gco = [ 'source-over','source-in','source-out','source-atop',
+            'destination-over','destination-in','destination-out','destination-atop',
+            'lighter', 'copy','xor', 'multiply', 'screen', 'overlay', 'darken',
+            'lighten', 'color-dodge', 'color-burn', 'hard-light', 'soft-light',
+            'difference', 'exclusion', 'hue', 'saturation', 'color', 'luminosity'
+          ]
+          console.log(gco)
+
+          gco.map(el => {
+              const o = document.createElement('option')
+              o.text = `${el}`
+              o.setAttribute('value', `${el}`)
+              selector.options.add(o)
+          })
+}
+
+getGlobalCompositeOperation()
 canvas.addEventListener('mousedown', (e) => {
     isDrawing = true;
     [lastX, lastY] = [e.offsetX, e.offsetY]
@@ -50,3 +81,5 @@ canvas.addEventListener('mousedown', (e) => {
 canvas.addEventListener('mousemove', draw)
 canvas.addEventListener('mouseup', () => isDrawing = false)
 canvas.addEventListener('mouseout', () => isDrawing = false)
+input.addEventListener('change', handleChangeInput)
+selector.addEventListener('change', handleChangeSelect)
